b3477ed99a9327b68d9d754e1f10870cf7b2badd
Christine Noonan <noo19002@byui.edu>
2021-05-03 21:21:28 -0400

    ta03

--- routes/ta03.js
+++ routes/ta03.js
@@ -2,13 +2,48 @@
 const express = require('express');
 const router = express.Router();
 
+const items = require('../public/data/ta03.json');
+
+const fs = require('fs');
+
+const path = require('path');
+
+const p = path.join(path.dirname(process.mainModule.filename), 'public', 'data', 'ta03.json');
+
+const getDataFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    }); 
+};
+
 router.get('/',(req, res, next) => {
-    res.render('pages/ta03', { 
-        title: 'Team Activity 03', 
-        path: '/ta03', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+    getDataFromFile(data => {
+        res.render('pages/ta03', { 
+                title: 'Team Activity 03', 
+                path: '/ta03', // For pug, EJS 
+                activeTA03: true, // For HBS
+                contentCSS: true, // For HBS
+                data: data
+            });
+            console.log(data);
     });
 });
+
+router.post('/', (req, res, next) => {
+    getDataFromFile(data => {
+        res.render('pages/ta03', { 
+                title: 'Team Activity 03', 
+                path: '/ta03', // For pug, EJS 
+                activeTA03: true, // For HBS
+                contentCSS: true, // For HBS
+                data: data.filter(item => item.tags.includes(req.body.search))
+            });
+            console.log(data);
+    });
+})
 
 module.exports = router;
